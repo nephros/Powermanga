74c330f14ae3d89d998a6c4b21b0240225307ef2
nephros <nemo@JollaC2>
2025-03-03 17:30:18 +0100

    Revert "Set render_accelerated flag"
    This reverts commit 768f5885a7255d393bc10c9903674b400c84094d.

--- src/display_sdl2.c
+++ src/display_sdl2.c
@@ -394,7 +394,7 @@ init_video_mode (void)
 
   Uint32 rflags;
   //rflags = SDL_RENDERER_TARGETTEXTURE | SDL_RENDERER_SOFTWARE;
-  rflags = SDL_RENDERER_TARGETTEXTURE | SDL_RENDERER_ACCELERATED;
+  rflags = SDL_RENDERER_TARGETTEXTURE;
   main_renderer = SDL_CreateRenderer(main_window, -1, rflags);
   if (main_renderer == NULL) 
     {
