a904328cda4fd5356d3ef08fa4c7fe1030251b5e
nephros <nemo@JollaC2>
2025-12-03 07:12:23 +0100

    Shift RGB masks

--- src/display_sdl2.c
+++ src/display_sdl2.c
@@ -2538,9 +2538,9 @@ get_rgb_mask (Uint32 * rmask, Uint32 * gmask, Uint32 * bmask)
       break;
     default:
 #if SDL_BYTEORDER == SDL_BIG_ENDIAN
-      *rmask = 0x00ff0000;
-      *gmask = 0x0000ff00;
-      *bmask = 0x000000ff;
+      *rmask = 0xff000000;
+      *gmask = 0x00ff0000;
+      *bmask = 0x0000ff00;
 #else
       *rmask = 0x000000ff;
       *gmask = 0x0000ff00;
